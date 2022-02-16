307c900bc035b1e1dd6c8572e52437bd43459f7b
Gregor <g@wbrck.de>
2022-02-16 17:03:06 +0100

    integrate aliase into fish.config and fix missing sh path

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,28 @@
 if status is-interactive
-    source ~/.config/aliases/aliases.sh
+    alias latex-update="sudo tlmgr update --self --all"
+    alias biber-cash-reset="rm -rf `biber --cache`"
+    alias cm='chezmoi'
+    alias cmcd='chezmoi cd'
+    alias cmgst='chezmoi git status'
+    alias cmgaa='chezmoi git add --all'
+    alias cmgc='chezmoi git commit -v'
+    alias cme='chezmoi edit'
+    alias g='git'
+    alias ga='git add'
+    alias gaa='git add --all'
+    alias gc='git commit -v'
+    alias gcl='git clone --recurse-submodules'
+    alias gd='git diff'
+    alias gss='git status -s'
+    alias gst='git status'
+    alias vi='nvim'
+    alias vim='nvim'
+    alias l='ls -la'
+    alias ..='cd ..'
+    alias ...='cd ../..'
+    alias ....='cd ../../..'
+    alias .....='cd ../../../..'
+    alias ......='cd ../../../../..'
     # Commands to run in interactive sessions can go here
 end
 

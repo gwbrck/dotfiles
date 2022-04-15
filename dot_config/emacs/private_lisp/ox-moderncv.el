17a957929a6f7687694ea32770154e8954a8afd7
Gregor <git.g@acc.wbrck.de>
2022-04-15 13:45:34 +0200

    general ulrs

--- dot_config/emacs/private_lisp/ox-moderncv.el
+++ dot_config/emacs/private_lisp/ox-moderncv.el
@@ -258,7 +258,7 @@ holding export options."
    "</ul>\n"
    (format
     "<a id=\"btn\" href=./%s.pdf download=\"CV_%s.pdf\"= ><i class=\"fa-solid fa-file-pdf\"></i> Download</a>"
-    (s-trim (replace-regexp-in-string "\\w+\\.de\\/" "" (org-export-data (plist-get info :homepage) info)))
+    (s-trim (replace-regexp-in-string "^.+\\/" "" (org-export-data (plist-get info :homepage) info)))
     (s-trim (replace-regexp-in-string " " "_" (org-export-data (plist-get info :author) info))))
    "</section>\n"
    ;; Document contents.

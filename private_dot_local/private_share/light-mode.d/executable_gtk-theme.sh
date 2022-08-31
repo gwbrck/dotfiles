e82c71deae86885d4200c175dc5b3b336f40c920
Gregor <g@wbrck.de>
2022-08-31 22:28:13 +0200

    sway_socket workarround

--- private_dot_local/private_share/light-mode.d/executable_gtk-theme.sh
+++ private_dot_local/private_share/light-mode.d/executable_gtk-theme.sh
@@ -1,4 +1,7 @@
 # Note: The names for the Arc theme variations are terrible.
 # "Darker" is actually LESS DARK than "Dark".
-
+export SWAYSOCK=/run/user/$(id -u)/sway-ipc.$(id -u).$(pgrep -x sway).sock
 gsettings set org.gnome.desktop.interface gtk-theme Arc-Lighter
+
+swaymsg client.focused "#5294E2 #5294E2 #FFFFFF #5294E2"
+swaymsg client.unfocused "#F5F6F7 #F5F6F7 #5C616C #F5F6F7 #F5F6F7"

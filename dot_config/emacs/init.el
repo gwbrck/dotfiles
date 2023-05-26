8b28c247e6ead826c46716e513f4869c3dc14216
gregor <git.g@acc.wbrck.de>
2023-05-26 19:43:42 +0200

    change manual treesit install to package

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -917,39 +917,12 @@ targets."
 (use-package eldoc-box
   :ensure t)
 
-(use-package treesit
-  :commands (treesit-install-language-grammar gwbrck/treesit-install-all-languages)
-  :init
-  (setq treesit-language-source-alist
-   '((bash . ("https://github.com/tree-sitter/tree-sitter-bash"))
-     (c . ("https://github.com/tree-sitter/tree-sitter-c"))
-     (cpp . ("https://github.com/tree-sitter/tree-sitter-cpp"))
-     (css . ("https://github.com/tree-sitter/tree-sitter-css"))
-     (go . ("https://github.com/tree-sitter/tree-sitter-go"))
-     (html . ("https://github.com/tree-sitter/tree-sitter-html"))
-     (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
-     (json . ("https://github.com/tree-sitter/tree-sitter-json"))
-     (lua . ("https://github.com/Azganoth/tree-sitter-lua"))
-     (make . ("https://github.com/alemuller/tree-sitter-make"))
-     (ocaml . ("https://github.com/tree-sitter/tree-sitter-ocaml" nil "ocaml/src"))
-     (python . ("https://github.com/tree-sitter/tree-sitter-python"))
-     (php . ("https://github.com/tree-sitter/tree-sitter-php"))
-     (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "tsx/src"))
-     (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" nil "typescript/src"))
-     (ruby . ("https://github.com/tree-sitter/tree-sitter-ruby"))
-     (rust . ("https://github.com/tree-sitter/tree-sitter-rust"))
-     (sql . ("https://github.com/m-novikov/tree-sitter-sql"))
-     (toml . ("https://github.com/tree-sitter/tree-sitter-toml"))
-     (zig . ("https://github.com/GrayJack/tree-sitter-zig"))))
+(use-package treesit-auto
+  :ensure t
+  :demand t
   :config
-  (defun gwbrck/treesit-install-all-languages ()
-    "Install all languages specified by `treesit-language-source-alist'."
-    (interactive)
-    (let ((languages (mapcar 'car treesit-language-source-alist)))
-      (dolist (lang languages)
-	      (treesit-install-language-grammar lang)
-	      (message "`%s' parser was installed." lang)
-	      (sit-for 0.75)))))
+  (setq treesit-auto-install 'prompt)
+  (global-treesit-auto-mode))
 
 (use-package eglot
   :config

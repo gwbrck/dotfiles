ff572c24a709e4b7976140117f04d2117dacd548
gregor <git.g@acc.wbrck.de>
2024-05-03 14:18:13 +0200

    use quarto-nvim-kickstarter

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,57 +1,151 @@
 return {
-        -- the colorscheme should be available when starting Neovim
-        {
-                "sainnhe/edge",
-                lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-                priority = 1000, -- make sure to load this before all the other start plugins
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      { "nvim-telescope/telescope-ui-select.nvim" },
+      {
+        "xvzc/chezmoi.nvim",
+        enabled = true,
+        dependencies = {
+          { "nvim-lua/plenary.nvim" },
         },
-        {
-                "f-person/auto-dark-mode.nvim",
-                lazy = false, 
-                priority = 100,
-                cond = vim.fn.has('Mac') == 1,
-                config = {
-                        update_interval = 1000,
-                        set_dark_mode = function()
-                                vim.api.nvim_set_option("background", "dark")
-                                vim.cmd("colorscheme edge")
-                        end,
-                        set_light_mode = function()
-                                vim.api.nvim_set_option("background", "light")
-                                vim.cmd("colorscheme edge")
-                        end,
-                },
-                init = function()
-                        require("auto-dark-mode").init()
-                end,
+      },
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      { "nvim-telescope/telescope-dap.nvim" },
+      {
+        "jmbuhr/telescope-zotero.nvim",
+        enabled = true,
+        dev = false,
+        dependencies = {
+          { "kkharji/sqlite.lua" },
         },
-        {
-                "nvim-lualine/lualine.nvim",
-                opts = {
-                        options = {
-                                component_separators = { left = '|', right = '|' },
-                                section_separators = { left = '', right = '' },
-                        },
+        config = function()
+          vim.keymap.set("n", "<leader>fz", ":Telescope zotero<cr>", { desc = "[z]otero" })
+        end,
+      },
+    },
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local previewers = require("telescope.previewers")
+      local new_maker = function(filepath, bufnr, opts)
+        opts = opts or {}
+        filepath = vim.fn.expand(filepath)
+        vim.loop.fs_stat(filepath, function(_, stat)
+          if not stat then
+            return
+          end
+          if stat.size > 100000 then
+            return
+          else
+            previewers.buffer_previewer_maker(filepath, bufnr, opts)
+          end
+        end)
+      end
 
-                },
+      local telescope_config = require("telescope.config")
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescope_config.values.vimgrep_arguments) }
+      -- I don't want to search in the `docs` directory (rendered quarto output).
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!docs/*")
+
+      telescope.setup({
+        defaults = {
+          buffer_previewer_maker = new_maker,
+          vimgrep_arguments = vimgrep_arguments,
+          file_ignore_patterns = {
+            "node_modules",
+            "%_files/",
+            "%_cache",
+            ".git/",
+            "site_libs",
+            ".venv",
+          },
+          layout_strategy = "flex",
+          sorting_strategy = "ascending",
+          layout_config = {
+            prompt_position = "top",
+          },
+          mappings = {
+            i = {
+              ["<C-u>"] = false,
+              ["<C-d>"] = false,
+              ["<esc>"] = actions.close,
+              ["<c-j>"] = actions.move_selection_next,
+              ["<c-k>"] = actions.move_selection_previous,
+            },
+          },
         },
-        {       "lewis6991/gitsigns.nvim",
-                event = { "BufReadPre", "BufNewFile" },
+        pickers = {
+          find_files = {
+            hidden = false,
+            find_command = {
+              "rg",
+              "--files",
+              "--hidden",
+              "--glob",
+              "!.git/*",
+              "--glob",
+              "!**/.Rpro.user/*",
+              "--glob",
+              "!_site/*",
+              "--glob",
+              "!docs/**/*.html",
+              "-L",
+            },
+          },
         },
-        {
-                "norcalli/nvim-colorizer.lua"
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
+          },
+          fzf = {
+            fuzzy = true, -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+          },
         },
-        {
-           "folke/which-key.nvim",
-           event = "VeryLazy",
-           init = function()
-              vim.o.timeout = true
-              vim.o.timeoutlen = 300
-           end,
-           opts = {
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-           }
-        },
+      })
+      telescope.load_extension("fzf")
+      telescope.load_extension("ui-select")
+      telescope.load_extension("dap")
+      telescope.load_extension("zotero")
+      vim.keymap.set("n", "<leader>cz", telescope.extensions.chezmoi.find_files, {})
+      telescope.load_extension("chezmoi")
+    end,
+  },
+
+  { -- Highlight todo, notes, etc in comments
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = { signs = false },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      options = {
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+      },
+    },
+  },
+  { "lewis6991/gitsigns.nvim", event = { "BufReadPre", "BufNewFile" } },
+  {
+    "norcalli/nvim-colorizer.lua",
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 }

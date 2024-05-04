928c23b583812939a7165e11e8e20d03edb8f0a4
gregor <g@wbrck.de>
2024-05-04 09:00:06 +0200

    formatted

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,11 @@
-eval "$(/opt/homebrew/bin/brew shellenv fish)"
+if status is-login
+    if test (uname) = Darwin
+        if test -d /opt/homebrew/bin
+            eval "$(/opt/homebrew/bin/brew shellenv fish)"
+        end
+    end
+end
+
 alias em='emacsclient -cn -a ""'
 if status is-interactive
     abbr tn "tmux new -s (pwd | sed 's/.*\///g')"
@@ -27,38 +34,38 @@ if status is-interactive
     alias .....='cd ../../../..'
     alias ......='cd ../../../../..'
     # Commands to run in interactive sessions can go here
-    set -U fish_color_autosuggestion      brblack
-    set -U fish_color_cancel              -r
-    set -U fish_color_command             brgreen
-    set -U fish_color_comment             brmagenta
-    set -U fish_color_cwd                 green
-    set -U fish_color_cwd_root            red
-    set -U fish_color_end                 brmagenta
-    set -U fish_color_error               brred
-    set -U fish_color_escape              brcyan
-    set -U fish_color_history_current     --bold
-    set -U fish_color_host                normal
-    set -U fish_color_match               --background=brblue
-    set -U fish_color_normal              normal
-    set -U fish_color_operator            cyan
-    set -U fish_color_param               brblue
-    set -U fish_color_quote               yellow
-    set -U fish_color_redirection         bryellow
-    set -U fish_color_search_match        'bryellow' '--background=brblack'
-    set -U fish_color_selection           'normal' '--bold' '--background=brblack'
-    set -U fish_color_status              red
-    set -U fish_color_user                brgreen
-    set -U fish_color_valid_path          --underline
-    set -U fish_pager_color_completion    normal
-    set -U fish_pager_color_description   yellow
-    set -U fish_pager_color_prefix        'normal' '--bold' '--underline'
-    set -U fish_pager_color_progress      'brwhite' '--background=cyan'
+    set -U fish_color_autosuggestion brblack
+    set -U fish_color_cancel -r
+    set -U fish_color_command brgreen
+    set -U fish_color_comment brmagenta
+    set -U fish_color_cwd green
+    set -U fish_color_cwd_root red
+    set -U fish_color_end brmagenta
+    set -U fish_color_error brred
+    set -U fish_color_escape brcyan
+    set -U fish_color_history_current --bold
+    set -U fish_color_host normal
+    set -U fish_color_match --background=brblue
+    set -U fish_color_normal normal
+    set -U fish_color_operator cyan
+    set -U fish_color_param brblue
+    set -U fish_color_quote yellow
+    set -U fish_color_redirection bryellow
+    set -U fish_color_search_match bryellow '--background=brblack'
+    set -U fish_color_selection normal --bold '--background=brblack'
+    set -U fish_color_status red
+    set -U fish_color_user brgreen
+    set -U fish_color_valid_path --underline
+    set -U fish_pager_color_completion normal
+    set -U fish_pager_color_description yellow
+    set -U fish_pager_color_prefix normal --bold --underline
+    set -U fish_pager_color_progress brwhite '--background=cyan'
     # Vi Keybindings
     set -U fish_key_bindings fish_vi_key_bindings
 end
 
 set fish_greeting ""
-set -Ux EDITOR "nvim"
+set -Ux EDITOR nvim
 
 # Ensure that GPG Agent is used as the SSH agent
 set -e SSH_AUTH_SOCK
@@ -73,8 +80,8 @@ set -Ux GPG_TTY $(tty)
 fish_add_path ~/bin
 fish_add_path ~/.local/bin #pipx
 
-set -Ux POETRY_CONFIG_DIR  $HOME/.config/pypoetry
+set -Ux POETRY_CONFIG_DIR $HOME/.config/pypoetry
 
-if test "$INSIDE_EMACS" = "vterm"
+if test "$INSIDE_EMACS" = vterm
     source {$EMACS_VTERM_PATH}etc/emacs-vterm.fish
 end

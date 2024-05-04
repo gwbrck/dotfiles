2dd6198757ca99c233e2c9009bffb7b88bfa3d38
gregor <g@wbrck.de>
2024-05-04 20:16:44 +0200

    add some defaults

--- .chezmoiscripts/run_once_before_11-mac-defaults.sh
+++ .chezmoiscripts/run_once_before_11-mac-defaults.sh
@@ -8,4 +8,13 @@ defaults write com.apple.dock launchanim -bool false
 defaults write com.apple.dock orientation right
 defaults write com.apple.dock show-recents -bool false
 
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+defaults write com.apple.dock expose-animation-duration -float 0.15
+
+defaults write com.apple.dock tilesize -int 64
+
 killall Dock

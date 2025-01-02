ea0472b2d7cca5dd7e84d77386cc356a5c20b7e7
gregor <g@wbrck.de>
2025-01-02 17:40:32 +0100

    change theme

--- dot_config/nvim/lua/plugins/colorthemes.lua
+++ dot_config/nvim/lua/plugins/colorthemes.lua
@@ -1,12 +1,8 @@
 return {
   {
-    "sainnhe/edge",
+    "EdenEast/nightfox.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000,
-    enabled = true,
-    config = function()
-      vim.cmd.colorscheme 'edge'
-    end
   },
   {
     "f-person/auto-dark-mode.nvim",
@@ -16,11 +12,11 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.o.background = "dark"
+        vim.cmd("colorscheme nightfox")
         -- vim.cmd("colorscheme edge")
       end,
       set_light_mode = function()
-        vim.o.background = "light"
+        vim.cmd("colorscheme dayfox")
         -- vim.cmd("colorscheme edge")
       end,
     },

51739ceb8331b16d426b02bbb5863722afc27a3e
Gregor Willenbrock <39911339+gwbrck@users.noreply.github.com>
2025-06-15 15:05:40 +0200

    Update run_once_before_10-mac-bootstrap.sh

--- .chezmoiscripts/run_once_before_10-mac-bootstrap.sh
+++ .chezmoiscripts/run_once_before_10-mac-bootstrap.sh
@@ -3,8 +3,11 @@ set -eufo pipefail
 xcode-select -p >/dev/null 2>&1 || xcode-select --install
 
 if ! type brew >/dev/null; then
+  echo "add path"
   export PATH="/opt/homebrew/bin:${PATH}"
+  echo "install brew"
   NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "brew installed"
 fi
 
 /opt/homebrew/bin/brew install git

fdcca2fe28d55df4240906381c7dc99b0c3373f7
gregor <git.g@acc.wbrck.de>
2026-03-24 12:56:29 +0100

    use uv for mcps and ocrmypdf

--- dot_config/uv/run_uv_tools.fish
+++ dot_config/uv/run_uv_tools.fish
@@ -3,3 +3,12 @@
 uv tool install pytr
 uv tool install kptncook
 uv tool install --with-executables-from ansible-core,ansible-lint --with requests ansible
+
+if test (uname) = Darwin
+    uv tool install ocrmypdf --with ocrmypdf-appleocr
+else
+    uv tool install ocrmypdf
+end
+
+uv tool install "zotero-mcp-server[all]"
+uv tool install basic-memory
